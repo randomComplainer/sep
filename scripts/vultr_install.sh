e86f68a0b5c6aa998037807f27d20958fdb280d1
zcdhp <zcdhp@nocontact.com>
2025-08-10 14:50:43 +0800

    config vsftpd

--- scripts/vultr_install.sh
+++ scripts/vultr_install.sh
@@ -12,3 +12,6 @@ useradd -m -d /srv/ftpdownloads -s /usr/sbin/nologin ftpuser;
 chmod 555 /srv/ftpdownloads
 chown ftpuser:ftpuser /srv/ftpdownloads
 
+sed -i 's/listen=NO/listen=YES/g' /etc/vsftpd.conf
+sed -i 's/listen_ipv6=YES/listen_ipv6=NO/g' /etc/vsftpd.conf
+sed -i 's/#chroot_local_user=YES/chroot_local_user=YES/g' /etc/vsftpd.conf
