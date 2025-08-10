0941f76fb2a9cfc891c6ae7006c6c06dcd742e9d
zcdhp <zcdhp@nocontact.com>
2025-08-10 15:26:23 +0800

    password

--- scripts/vultr_install.sh
+++ scripts/vultr_install.sh
@@ -2,16 +2,22 @@
 
 set -e
 
+
 apt-get update;
-apt-get install -y \
-	vsftpd;
 
+if ! dpkg -s vsftpd; then
+	apt-get install -y vsftpd;
+fi
 
-useradd -m -d /srv/ftpdownloads -s /usr/sbin/nologin ftpuser;
-(echo "opensesame"; echo "opensesame") passwd ftpuser;
-chmod 555 /srv/ftpdownloads
-chown ftpuser:ftpuser /srv/ftpdownloads
+if ! id -u ftpuser; then
+	useradd -m -d /srv/ftpdownloads -s /usr/sbin/nologin ftpuser;
+	(echo "sep-user"; echo "sep-user") passwd ftpuser;
+	chmod 555 /srv/ftpdownloads
+	chown ftpuser:ftpuser /srv/ftpdownloads
+fi
 
 sed -i 's/listen=NO/listen=YES/g' /etc/vsftpd.conf
 sed -i 's/listen_ipv6=YES/listen_ipv6=NO/g' /etc/vsftpd.conf
 sed -i 's/#chroot_local_user=YES/chroot_local_user=YES/g' /etc/vsftpd.conf
+
+systemctl restart vsftpd
