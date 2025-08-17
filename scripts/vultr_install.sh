b0d895996cc6f4ea3d7f3b007367ff2212b13e7f
zcdhp <zcdhp@nocontact.com>
2025-08-17 15:37:47 +0800

    remote build

--- scripts/vultr_install.sh
+++ scripts/vultr_install.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-
 apt-get update;
 
 if ! dpkg -s vsftpd; then
@@ -18,9 +17,32 @@ fi
 if ! id -u ftpuser; then
 	useradd -m -d /srv/ftpdownloads -s /usr/sbin/nologin ftpuser;
 	(echo "sep-user"; echo "sep-user") | passwd ftpuser;
-	chmod 555 /srv/ftpdownloads
-	chown ftpuser:ftpuser /srv/ftpdownloads
+	chmod 555 /srv/ftpdownloads;
+	chown ftpuser:ftpuser /srv/ftpdownloads;
 fi
 
-
 systemctl restart vsftpd
+
+if ! -x "$(which rustup)"; then
+	apt-get install -y rustup;
+	rustup update nightly;
+	apt install mingw-w64;
+	rustup target add x86_64-pc-windows-gnu;
+	rustup default nightly;
+fi
+
+if ! -e /swapfile; then
+	fallocate -l 2G /swapfile
+	chmod 600 /swapfile
+	mkswap /swapfile
+	swapon /swapfile
+fi
+
+cargo build --release;
+cargo build --release --target x86_64-pc-windows-gnu;
+
+cp ./target/release/sep-client /srv/ftpdownloads/;
+cp ./target/x86_64-pc-windows-gnu/release/sep-client.exe /srv/ftpdownloads/;
+
+# wget "ftp://ftpuser:sep-user@{}/sep-client" 
+
